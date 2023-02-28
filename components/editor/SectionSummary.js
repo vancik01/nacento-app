2039c10f97a2c0904cb1306fec1e963019bb4f92
Martin Vančo <martinvanco@eduroam-162-238.fiit.stuba.sk>
2023-02-28 18:42:12 +0100

    fixed section generating

--- components/editor/SectionSummary.js
+++ components/editor/SectionSummary.js
@@ -1,14 +1,17 @@
 import { TextareaAutosize } from "@mui/material";
 import React from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import { useData } from "../../context/AppWrap";
 import { useLayout } from "../../context/LayoutContext";
 import AddRow from "../../public/SVG/AddRow";
+import Cancel from "../../public/SVG/Cancel";
+import DragableIcon from "../../public/SVG/Dragable";
 import AddSection from "./AddSection";
 import TestElement from "./TestElement";
 
 export default function SectionSummary({ blocks, sectionId, sectionsLength }) {
 	const { primaryColor } = useLayout();
-	const { addBlock, editBlockTitle, test, settest } = useData();
+	const { addBlock, editBlockTitle, deleteBlock } = useData();
 	return (
 		<div className="mt-4 table_wrap">
 			{blocks.length > 0 && (
@@ -26,42 +29,76 @@ export default function SectionSummary({ blocks, sectionId, sectionsLength }) {
 					<div className="pl-2">Cena Celkom</div>
 				</div>
 			)}
+			<DragDropContext
+				onDragEnd={() => {
+					console.log("drag-block");
+				}}
+			>
+				<Droppable droppableId={`table`}>
+					{(provided) =>
+						blocks.map((block, blockId) => {
+							return (
+								<Draggable>
+									{(provided, snapshot) => (
+										<div
+											{...provided.droppableProps}
+											ref={provided.innerRef}
+											key={blockId}
+											className={`grid table_row content relative ${
+												blockId === blocks.length - 1 ? "last" : ""
+											}`}
+											style={{
+												gridTemplateColumns: "50px 1fr 140px 140px 140px",
+											}}
+										>
+											<div className="h-full flex items-center justify-start py-1 px-2 table_unit">
+												{blockId + 1}
+											</div>
+											<div className="h-full flex items-center justify-start py-1 px-2 table_unit">
+												<TextareaAutosize
+													className="w-full bg-transparent focus-visible:outline-none h-fit overflow-visible"
+													value={block.info.title}
+													onChange={(e) => {
+														editBlockTitle(e.target.value, sectionId, blockId);
+													}}
+													style={{ resize: "none" }}
+												/>
+											</div>
+											<div className="h-full flex items-center justify-start py-1 px-2 table_unit">
+												{parseFloat(
+													block.info.total_construction_price
+												).toFixed(2)}{" "}
+												€
+											</div>
 
-			{blocks.map((block, blockId) => {
-				return (
-					<div
-						key={blockId}
-						className={`grid table_row content ${
-							blockId === blocks.length - 1 ? "last" : ""
-						}`}
-						style={{ gridTemplateColumns: "50px 1fr 140px 140px 140px" }}
-					>
-						<div className="h-full flex items-center justify-start py-1 px-2 table_unit">
-							{blockId + 1}
-						</div>
-						<div className="h-full flex items-center justify-start py-1 px-2 table_unit">
-							<TextareaAutosize
-								className="w-full bg-transparent focus-visible:outline-none h-fit overflow-visible"
-								value={block.info.title}
-								onChange={(e) => {
-									editBlockTitle(e.target.value, sectionId, blockId);
-								}}
-								style={{ resize: "none" }}
-							/>
-						</div>
-						<div className="h-full flex items-center justify-start py-1 px-2 table_unit">
-							{parseFloat(block.info.total_construction_price).toFixed(2)} €
-						</div>
+											<div className="h-full flex items-center justify-start py-1 px-2 table_unit">
+												{parseFloat(block.info.total_delivery_price).toFixed(2)}{" "}
+												€
+											</div>
+											<div className="h-full flex items-center justify-start py-1 px-2 table_unit">
+												{parseFloat(block.info.total).toFixed(2)} €
+											</div>
 
-						<div className="h-full flex items-center justify-start py-1 px-2 table_unit">
-							{parseFloat(block.info.total_delivery_price).toFixed(2)} €
-						</div>
-						<div className="h-full flex items-center justify-start py-1 px-2 table_unit">
-							{parseFloat(block.info.total).toFixed(2)} €
-						</div>
-					</div>
-				);
-			})}
+											<div className="flex justify-end gap-1 select-none absolute -right-20">
+												<div
+													onClick={() => {
+														deleteBlock(sectionId, blockId);
+													}}
+												>
+													<Cancel color={primaryColor} />
+												</div>
+												<div>
+													<DragableIcon />
+												</div>
+											</div>
+										</div>
+									)}
+								</Draggable>
+							);
+						})
+					}
+				</Droppable>
+			</DragDropContext>
 			<button
 				onClick={() => {
 					addBlock(sectionId);
