626ed444c3971df6f3696d58321cf6c38aa88243
Martin Vančo <martinvanco@Martins-MacBook-Pro.local>
2023-03-12 23:18:20 +0100

    separated

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,21 @@
 import "react-tooltip/dist/react-tooltip.css";
-import React from "react";
+import React, { useEffect } from "react";
 
 import HomeArea from "../components/landing_page/HomeArea";
 import About from "../components/landing_page/About";
 import Functions from "../components/landing_page/Functions";
 import Footer from "../components/landing_page/Footer";
+import { useRouter } from "next/router";
 
 export default function Home() {
+	const router = useRouter();
+	useEffect(() => {
+		router.push("/cenova-ponuka/select-project/");
+	}, []);
+
 	return (
 		<>
-			<HomeArea />
-			<About />
-			<Functions />
-			<Footer />
+			<div>Loading...</div>
 		</>
 	);
 }
